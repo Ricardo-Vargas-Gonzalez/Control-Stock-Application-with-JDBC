54680b267af3de9159af5d0b1dad38bdc1b035b8
Ricardo-Vargas-Gonzalez <ceinnova05251@innovaccion.mx>
2023-09-12 23:23:20 -0600

    Try With Resources

--- COM.ALURA/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ COM.ALURA/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -14,107 +14,133 @@ import com.alura.jdbc.factory.ConnectionFactory;
 
 public class ProductoController {
 
-	public int modificar(String nombre, String descripcion, Integer cantidad, Integer id) throws SQLException {
+    public int modificar(String nombre, String descripcion, Integer cantidad, Integer id) throws SQLException {
         ConnectionFactory factory = new ConnectionFactory();
-        Connection con = factory.recuperaConexion();
+        final Connection con = factory.recuperaConexion();
 
-        PreparedStatement statement = con.prepareStatement(
-                "UPDATE PRODUCTO SET "
-                + " NOMBRE = ?"
-                + ", DESCRIPCION = ?"
-                + ", CANTIDAD = ?" 
-                + " WHERE ID = ?" );
-        statement.setString(1, nombre);
-        statement.setString(2, descripcion);
-        statement.setInt(3, cantidad);
-        statement.setInt(4, id);
+        try (con) {
 
-        statement.execute();
-    
-        int updateCount = statement.getUpdateCount();
-    
-        con.close();
-    
-        return updateCount;
+            final PreparedStatement statement = con.prepareStatement(
+                    "UPDATE PRODUCTO SET "
+                            + " NOMBRE = ?"
+                            + ", DESCRIPCION = ?"
+                             + ", CANTIDAD = ?"
+                            + " WHERE ID = ?");
+            try (statement) {
+                statement.setString(1, nombre);
+                statement.setString(2, descripcion);
+                statement.setInt(3, cantidad);
+                statement.setInt(4, id);
+
+                statement.execute();
+
+                int updateCount = statement.getUpdateCount();
+
+                return updateCount;
+            }
+        }
     }
 
-	public int eliminar(Integer id) throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
-        
-        PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
-        statement.setInt(1, id);
-        statement.execute();
+    public int eliminar(Integer id) throws SQLException {
+        ConnectionFactory factory = new ConnectionFactory();
+        final Connection con = factory.recuperaConexion();
 
-        return statement.getUpdateCount();
-	}
+        try (con) {
+            final PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
 
-	public List<Map<String, String>> listar() throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
+            try (statement) {
+                statement.setInt(1, id);
+                statement.execute();
 
-		
-		PreparedStatement statement = con.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
-        statement.execute();
-
-        ResultSet resultSet = statement.getResultSet();
-
-        List<Map<String, String>> resultado = new ArrayList<>();
-
-        while (resultSet.next()) {
-            Map<String, String> fila = new HashMap<>();
-            fila.put("ID", String.valueOf(resultSet.getInt("ID")));
-            fila.put("NOMBRE", resultSet.getString("NOMBRE"));
-            fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
-            fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
-
-            resultado.add(fila);
+                return statement.getUpdateCount();
+            }
         }
+    }
 
-        con.close();
+    public List<Map<String, String>> listar() throws SQLException {
 
-        return resultado;
+        ConnectionFactory factory = new ConnectionFactory();
+        final Connection con = factory.recuperaConexion();
+
+        try (con) {
+
+            final PreparedStatement statement = con
+                    .prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+            try (statement) {
+                statement.execute();
+
+                ResultSet resultSet = statement.getResultSet();
+
+                List<Map<String, String>> resultado = new ArrayList<>();
+
+                while (resultSet.next()) {
+                    Map<String, String> fila = new HashMap<>();
+                    fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+                    fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+                    fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+                    fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+
+                    resultado.add(fila);
+                }
+                return resultado;
+            }
+        }
     }
 
     public void guardar(Map<String, String> producto) throws SQLException {
-		String nombre = producto.get("NOMBRE");
+        String nombre = producto.get("NOMBRE");
         String descripcion = producto.get("DESCRIPCION");
         Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
         Integer maximoCantidad = 50;
-        
-        Connection con = new ConnectionFactory().recuperaConexion();
-        con.setAutoCommit(false);
 
-        PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO "+ "(nombre, descripcion, cantidad)"
-        + " VALUES(?, ?, ?)",
-            Statement.RETURN_GENERATED_KEYS);
+        ConnectionFactory factory = new ConnectionFactory();
+        final Connection con = factory.recuperaConexion();
+        try (con) {
+            con.setAutoCommit(false);
 
-    do {
-        int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
+            final PreparedStatement statement = con.prepareStatement(
+                    "INSERT INTO PRODUCTO " + "(nombre, descripcion, cantidad)"
+                            + " VALUES(?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
-        ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
+            try (statement) {
 
-        cantidad -= maximoCantidad;
-    }while(cantidad > 0);
+                do {
+                    int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
 
-        con.close();
-	}
+                    ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
+
+                    cantidad -= maximoCantidad;
+                } while (cantidad > 0);
+
+                con.commit();
+                System.out.println("COMMIT");
+            } catch (Exception e) {
+                con.rollback();
+                System.out.println("ROLLBACK");
+            }
+        }
+    }
 
     private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
             throws SQLException {
         statement.setString(1, nombre);
-        
+
         statement.setInt(3, cantidad);
         statement.setString(2, descripcion);
-        
 
         statement.execute();
-            
-        ResultSet resultSet = statement.getGeneratedKeys();
 
-        while(resultSet.next()) {
-            System.out.println(
-                String.format("Fue insertado el producto de ID %d",
-                resultSet.getInt(1)));
-            
+        final ResultSet resultSet = statement.getGeneratedKeys();
+
+        try (resultSet) {
+
+            while (resultSet.next()) {
+                System.out.println(
+                        String.format("Fue insertado el producto de ID %d",
+                                resultSet.getInt(1)));
+            }
         }
     }
 
--- README.md
+++ README.md
@@ -21,3 +21,9 @@ Siempre que se abre una conexion debe cerrarse de lo contrario la misma se conti
 Finalmente entendí el termino, se trada de usar comandos de sql en la aplicacion desarrollada generando su fallo de manera, simple o drástica, esto puede ocurrir por errores de tipado del usuario, algo común o en los casos mas peligrosos, cuando un atacante busca el daño de la aplicación, esto puede afectar por completo el trabajop de producción, por lo que hay que ser cautelosos en lo que se permite y como se trata con estos tipos de comandos.
 En el caso de java podemos tratar esta vulnerabilidad por medio de los prepareStatements que directamente convertiran cualquier intento de comando sql en los campos de la aplicación, en mero texto añadido a la tabla de MySql.
 El PreparedStatement mantiene la query compilada en la base de datos, de forma parametrizada. Así el usuario puede ejecutar la misma consulta diversas veces con parámetros distintos.
+
+/*
+             * El estándar de JDBC (del driver) para manejar transacciones de base de datos
+             * es el Auto-Commit que puede ser modificado por el método setAutoCommit,
+             * de la interfaz Connection
+             */
